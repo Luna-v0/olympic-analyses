1d7b5b45e20766e31c4d5476c5c0570033a29f89
PaMeirelles <rafaelpmeirelles@gmail.com>
2024-09-03 18:28:22 -0300

    Raw explicability

--- polished_dataset.py
+++ polished_dataset.py
@@ -9,8 +9,11 @@ df.dropna(subset=columns_to_drop, inplace=True)
 df.fillna("No Medal", inplace=True)
 df = df[df.Season == "Summer"]
 
-sports_few_data_points = ["Motorboating", "Figure Skating", "Lacrosse"]
+sports_few_data_points = ["Motorboating", "Figure Skating", "Lacrosse", "Ice Hockey", "Rugby", "Tug-Of-War",
+                           "Art Competitions"]
 df = df[~df.Sport.isin(sports_few_data_points)]
-df = df.drop(columns=["ID", "Name", "Team", "Games", "City", "Season"])
 
-df.to_csv("data/polished.csv")
+df["Won Medal"] = df.Medal != "No Medal"
+df["BMI"] = df["Weight"] / (df["Height"] / 100) ** 2
+print(df.head(), df.shape)
+df.to_csv("data/polished2.csv")
