8023b61add40e98eb7fca79c6699cf117230e170
Ajnus <ajnasoares@hotmail.com>
2024-10-20 18:13:56 -0300

    implementa botões de navegações entre página no topo, modulariza aplicação em torno de app.py, deleta site.py, deleta website.py, deleta site2_cssnovo (viro site2.py), imprime "Loading pergunta_X layout..." altera .gitgnore

--- nn.py
+++ nn.py
@@ -57,10 +57,10 @@ def recalculate_coords(dataframe, attributes, method='MDS', medal_multiplier=2):
 event_options = [{"label": event, "value": event} for event in df["Event"].unique()]
 sport_options = [{"label": sport, "value": sport} for sport in df["Sport"].unique()]
 
-app = dash.Dash(__name__)
+#app = dash.Dash(__name__)
 
-# Layout of the Dash app
-app.layout = html.Div([
+print("Loading pergunta_3 layout...")
+layout = html.Div([
     html.H1("Sport Similarity", style={'textAlign': 'center', 'marginBottom': '30px'}),
 
     html.Div([
@@ -132,86 +132,88 @@ app.layout = html.Div([
     html.Div(id='stress-metric', style={'textAlign': 'center', 'marginTop': '20px'})
 ], style={'width': '80%', 'margin': '0 auto'})
 
-# Callback to update the graph
-@app.callback(
-    [Output('scatter-plot', 'figure'),
-     Output('stress-metric', 'children')],
-    [Input('sport-selector', 'value'),  # Sport selection input
-     Input('event-selector', 'value'),  # Event selection input
-     Input('attribute-selector', 'value'),  # Feature selection input
-     Input('medal-multiplier-input', 'value'),  # Medal multiplier input
-     Input('method-selector', 'value'),  # MDS or PCA selection
-     Input('view-selector', 'value')]  # 2D or 3D view selection
-)
-def update_graph(selected_sport, selected_event, selected_attributes, medal_multiplier, selected_method, view_selector):
-    recalculated_coords, stress = recalculate_coords(df, selected_attributes, selected_method, medal_multiplier)
+def register_callbacks(app):
+    # Callback to update the graph
+    @app.callback(
+        [Output('scatter-plot', 'figure'),
+        Output('stress-metric', 'children')],
+        [Input('sport-selector', 'value'),  # Sport selection input
+        Input('event-selector', 'value'),  # Event selection input
+        Input('attribute-selector', 'value'),  # Feature selection input
+        Input('medal-multiplier-input', 'value'),  # Medal multiplier input
+        Input('method-selector', 'value'),  # MDS or PCA selection
+        Input('view-selector', 'value')]  # 2D or 3D view selection
+    )
+    def update_graph(selected_sport, selected_event, selected_attributes, medal_multiplier, selected_method, view_selector):
+        recalculated_coords, stress = recalculate_coords(df, selected_attributes, selected_method, medal_multiplier)
 
-    if '3D' in view_selector:
-        fig = px.scatter_3d(
-            recalculated_coords,
-            x='x',  # x coordinates
-            y='y',  # y coordinates
-            z='z',  # z coordinates for 3D plotting
-            hover_data=['Event', 'Sport'] + selected_attributes,
-            title="Dimensionality Reduction Plot (3D)"
-        )
+        if '3D' in view_selector:
+            fig = px.scatter_3d(
+                recalculated_coords,
+                x='x',  # x coordinates
+                y='y',  # y coordinates
+                z='z',  # z coordinates for 3D plotting
+                hover_data=['Event', 'Sport'] + selected_attributes,
+                title="Dimensionality Reduction Plot (3D)"
+            )
 
-        fig.update_layout(
-            scene=dict(
+            fig.update_layout(
+                scene=dict(
+                    xaxis_title='X Coordinate',
+                    yaxis_title='Y Coordinate',
+                    zaxis_title='Z Coordinate'
+                ),
+                hovermode='closest',
+                title={'text': f"{selected_method} Plot of Bio By Sport Male (3D)", 'x': 0.5, 'xanchor': 'center'}
+            )
+
+        else:
+            # Plot 2D if '3D' is not in view_selector
+            fig = px.scatter(
+                recalculated_coords,
+                x='x',  # x coordinates
+                y='y',  # y coordinates
+                hover_data=['Event', 'Sport'] + selected_attributes,
+                title="Dimensionality Reduction Plot (2D)"
+            )
+
+            fig.update_layout(
                 xaxis_title='X Coordinate',
                 yaxis_title='Y Coordinate',
-                zaxis_title='Z Coordinate'
-            ),
-            hovermode='closest',
-            title={'text': f"{selected_method} Plot of Bio By Sport Male (3D)", 'x': 0.5, 'xanchor': 'center'}
-        )
+                hovermode='closest',
+                title={'text': f"{selected_method} Plot of Bio By Sport Male (2D)", 'x': 0.5, 'xanchor': 'center'}
+            )
 
-    else:
-        # Plot 2D if '3D' is not in view_selector
-        fig = px.scatter(
-            recalculated_coords,
-            x='x',  # x coordinates
-            y='y',  # y coordinates
-            hover_data=['Event', 'Sport'] + selected_attributes,
-            title="Dimensionality Reduction Plot (2D)"
-        )
-
-        fig.update_layout(
-            xaxis_title='X Coordinate',
-            yaxis_title='Y Coordinate',
-            hovermode='closest',
-            title={'text': f"{selected_method} Plot of Bio By Sport Male (2D)", 'x': 0.5, 'xanchor': 'center'}
-        )
-
-    # Custom coloring and sizing for selected sport/event
-    if selected_sport or selected_event:
-        fig.update_traces(
-            marker=dict(color=[
-                "green" if sport == selected_sport else (
-                    "red" if event == selected_event else "blue")
-                for sport, event in zip(recalculated_coords["Sport"], recalculated_coords["Event"])
-            ],
-                size=[
-                    14 if sport == selected_sport else (
-                        12 if event == selected_event else 8)
+        # Custom coloring and sizing for selected sport/event
+        if selected_sport or selected_event:
+            fig.update_traces(
+                marker=dict(color=[
+                    "green" if sport == selected_sport else (
+                        "red" if event == selected_event else "blue")
                     for sport, event in zip(recalculated_coords["Sport"], recalculated_coords["Event"])
-                ])
+                ],
+                    size=[
+                        14 if sport == selected_sport else (
+                            12 if event == selected_event else 8)
+                        for sport, event in zip(recalculated_coords["Sport"], recalculated_coords["Event"])
+                    ])
+            )
+
+        # Add hover template for custom display
+        fig.update_traces(
+            hovertemplate="<b>%{customdata[0]}</b><br>Sport: %{customdata[1]}<br>" +
+                        "<br>".join([f"{attr}: %{{customdata[{i+2}]}}" for i, attr in enumerate(selected_attributes)])
         )
 
-    # Add hover template for custom display
-    fig.update_traces(
-        hovertemplate="<b>%{customdata[0]}</b><br>Sport: %{customdata[1]}<br>" +
-                      "<br>".join([f"{attr}: %{{customdata[{i+2}]}}" for i, attr in enumerate(selected_attributes)])
-    )
+        if selected_method == 'MDS':
+            stress_message = f"Stress value for MDS: {stress:.4f}"
+        else:
+            stress_message = "PCA does not calculate stress."
 
-    if selected_method == 'MDS':
-        stress_message = f"Stress value for MDS: {stress:.4f}"
-    else:
-        stress_message = "PCA does not calculate stress."
-
-    return fig, stress_message
+        return fig, stress_message
 
 
 # Run the app
-if __name__ == '__main__':
-    app.run_server(debug=True)
+#if __name__ == '__main__':
+    #app.run_server(debug=True)
+    
