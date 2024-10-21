fb50a7e011f8ccec98d83f997e63d7e7c58d5543
Eduardo Dantas Luna <michaelthefear@gmail.com>
2024-10-21 08:33:01 -0300

    Cleaning repo 2

---
+++ data_analyses/data_transformers/polished_dataset.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+base_file_path = '../../data/'
+
+df = pd.read_csv(base_file_path+"athlete_events.csv")
+
+columns_to_drop = ['ID', 'Name', 'Sex', 'Age', 'Height', 'Weight', 'Team', 'NOC', 'Games',
+       'Year', 'Season', 'City', 'Sport', 'Event']
+df.dropna(subset=columns_to_drop, inplace=True)
+
+df.fillna("No Medal", inplace=True)
+df = df[df.Season == "Summer"]
+
+sports_few_data_points = ["Motorboating", "Figure Skating", "Lacrosse", "Ice Hockey", "Rugby", "Tug-Of-War",
+                           "Art Competitions"]
+df = df[~df.Sport.isin(sports_few_data_points)]
+
+df["Won Medal"] = df.Medal != "No Medal"
+df["BMI"] = df["Weight"] / (df["Height"] / 100) ** 2
+print(df.head(), df.shape)
+df.to_csv(base_file_path+"polished2.csv")
---
+++ data_analyses/data_transformers/polished_dataset_2.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+base_file_path = '../../data/'
+
+
+df = pd.read_csv(base_file_path+"polished2.csv")
+
+filtered_df = df.groupby("Event").filter(lambda x: len(x) >= 10)
+
+filtered_df.to_csv(base_file_path+"polished3.csv", index=False)
