293c2a6b77e02b53ccf1b9a3a77c98e41b3943de
PaMeirelles <rafaelpmeirelles@gmail.com>
2024-11-20 20:27:40 -0300

    Fixed event and sport csv

--- data_analyses/data_transformers/get_by_sport.py
+++ data_analyses/data_transformers/get_by_sport.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-columns = ['Age', 'Height', 'BMI', 'GDP', 'Sport']
+columns = ['Age', 'Height', 'BMI', 'GDP', 'Sport', 'Sex']
 df = pd.read_csv("../data/features.csv")
 df = df[columns]
-aggregated_df = df.groupby('Sport', as_index=False).mean()
+aggregated_df = df.groupby(['Sport', 'Sex'], as_index=False).mean()
 aggregated_df.to_csv("../data/by_sport.csv", index=False)
